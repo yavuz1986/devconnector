6eaae0a6eed7b695198ec9f6eb4e9ebe657dac96
Yavuz YILDIZ <yavuzyildiz@Yavuz-MacBook-Pro.local>
2018-11-19 16:36:04 +0300

    server 80 port

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.use('/api/posts', posts);
   });
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

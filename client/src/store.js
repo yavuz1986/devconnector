28cba4f25ad2da128fa0a38f34b2ed0c3b076902
Yavuz YILDIZ <yavuzyildiz@Yavuz-MacBook-Pro.local>
2018-11-20 14:11:04 +0300

    dev tools removed

--- client/src/store.js
+++ client/src/store.js
@@ -7,11 +7,11 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-  rootReducer, 
-  initialState, 
+  rootReducer,
+  initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
